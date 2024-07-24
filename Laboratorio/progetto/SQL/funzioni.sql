825e8165b9410f9722473eb643a775d8b333dd42
Giacomo Castellana <giacomo.castellana01@gmail.com>
2024-07-24 17:46:50 +0200

    aggiornamento della ricerca per titolo

--- Laboratorio/progetto/SQL/funzioni.sql
+++ Laboratorio/progetto/SQL/funzioni.sql
@@ -253,7 +253,7 @@ begin
         select titolo, id, indirizzo
         from biblioteca.libro l inner join biblioteca.copia c on l.isbn=c.libro inner join            
         biblioteca.biblioteca b on b.sede=c.dove
-        where LOWER(l.titolo) LIKE (LOWER(tiRic)||'%') and b.sede<>'00000';
+        where LOWER(l.titolo) LIKE ('%'||LOWER(tiRic)||'%') and b.sede<>'00000';
 end;
 $$ language 'plpgsql';
 
