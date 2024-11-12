6e6664496dba518b1d37c2b0e4b2b5cbfc131b3b
Giacomo Castellana <giacomo.castellana01@gmail.com>
2024-11-12 17:47:00 +0100

    Update conf.php

--- Laboratorio/progetto/BDLAB_11970A_CastellanaGiacomo/Sorgenti/Sito/conf/conf.php
+++ Laboratorio/progetto/BDLAB_11970A_CastellanaGiacomo/Sorgenti/Sito/conf/conf.php
@@ -1,6 +1,6 @@
 <?php
 define ("myhost", "postgres");
 define ("myuser", "giacomo_castellana");
-define ("mypsw", "bZQKSMWL9x5Ugb5!");
+define ("mypsw", "EHVOLEVI");
 define ("mydb", "giacomo_castellana_lab");
 ?>
